7fe88893c0f8962e08adf359d47bd5e05ac355e7
m.chorakchi <m.chorakchi@gmail.com>
2019-09-07 14:03:27 +0200

    googlemap and set a keyAPI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+function App({...props}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Map
+        google={props.google}
+        zoom={14}
+        style={mapStyles}
+
+      />
     </div>
   );
 }
 
-export default App;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCPKCrczVJ4mA1VuVNdQxq-q0FrsJNUTgk'
+})(App);
